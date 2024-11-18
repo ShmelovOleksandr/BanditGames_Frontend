3e08389b84ff8dfa9d265c48efe8a1a3e5b1af4d
anna.deineko <anna.deineko@student.kdg.be>
2024-11-18 22:15:53 +0100

    feat: pallete added

--- bandit-games/tailwind.config.js
+++ bandit-games/tailwind.config.js
@@ -9,7 +9,23 @@ export default {
 
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                danger: {
+                    10: "#000000",
+                    50: "#310413",
+                    100: "#610726",
+                    200: "#920b3a",
+                    300: "#c20e4d",
+                    400: "#f31260",
+                    500: "#f51480",
+                    600: "#f817a0",
+                    700: "#faa0bf",
+                    800: "#fdd0df",
+                    900: "#fee7ef",
+                },
+            },
+        }
     },
     darkMode: "class",
     plugins: [nextui()]
