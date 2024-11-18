b2475151125c76cffa694e82e82aac33b425820f
anna.deineko <anna.deineko@student.kdg.be>
2024-11-18 20:56:04 +0100

    upd: vite project for bandit games platform created

--- gamePlatform/src/App.tsx
+++ gamePlatform/src/App.tsx
@@ -1,11 +1,10 @@
+import React from 'react'
 import './App.css'
-import {Button} from "@nextui-org/button";
 
 function App() {
     return (
-        <Button color="primary">
-            Button
-        </Button>
+        <div></div>
+
     )
 }
 
