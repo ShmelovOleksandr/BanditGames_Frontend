ddc4f12ac3beca424d0a828a77a830a8388e0c9b
anna.deineko <anna.deineko@student.kdg.be>
2024-11-18 22:16:11 +0100

    feat: NavBar component

--- bandit-games/src/App.tsx
+++ bandit-games/src/App.tsx
@@ -1,45 +1,8 @@
 import './App.css'
-import {Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/react";
-
+import {Navigation} from '@/components/Navbar/index.tsx'
 function App() {
     return (
-        <>
-            <Navbar>
-                <NavbarBrand>
-                    {/*<AcmeLogo/>*/}
-                    <p className="font-bold text-inherit">ACME</p>
-                </NavbarBrand>
-                <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                    <NavbarItem>
-                        <Link color="foreground" href="#">
-                            Features
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem isActive>
-                        <Link href="#" aria-current="page">
-                            Customers
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link color="foreground" href="#">
-                            Integrations
-                        </Link>
-                    </NavbarItem>
-                </NavbarContent>
-                <NavbarContent justify="end">
-                    <NavbarItem className="hidden lg:flex">
-                        <Link href="#">Login</Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Button as={Link} color="primary" href="#" variant="flat">
-                            Sign Up
-                        </Button>
-                    </NavbarItem>
-                </NavbarContent>
-            </Navbar>            <h1 className="text-3xl font-bold underline bg-amber-700">
-            Hello world!
-        </h1>
-        </>
+          <Navigation/>
     )
 }
 
---
+++ bandit-games/src/components/Navbar/index.tsx
@@ -0,0 +1,39 @@
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+export const Navigation = () => {
+    return (
+        <Navbar shouldHideOnScroll className="bg-danger-300 text-white">
+            <NavbarBrand>
+                <p className="font-bold text-inherit">LOGO</p>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        My Account
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="#" aria-current="page">
+                        Home
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Game Library
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    )
+}
+export default Navigation
