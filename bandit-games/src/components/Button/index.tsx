8bd205ba23d6ad37ebfe31f3bc6a821aeacf213b
anna.deineko <anna.deineko@student.kdg.be>
2024-11-23 17:49:17 +0100

    feat: error page gif

---
+++ bandit-games/src/components/Button/index.tsx
@@ -0,0 +1,21 @@
+import {Button} from "@nextui-org/button";
+import {useNavigate} from "react-router-dom";
+
+interface ButtonType {
+    link: string;
+    text: string;
+}
+
+const ButtonComponent: React.FC<ButtonType> = ({link, text}) => {
+    const navigate = useNavigate();
+    return (
+        <Button
+            className="text-sm font-normal text-default-600 bg-default-100"
+            variant="flat"
+            onClick={() => navigate(link)}
+        >
+            {text}
+        </Button>
+    )
+}
+export default ButtonComponent
