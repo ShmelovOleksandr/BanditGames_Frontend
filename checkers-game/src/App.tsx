467c3aaf462b53da69aff3edf7a37a4a0d406e72
Mykhailo Kruts <mykhailo.kruts@student.kdg.be>
2024-12-15 12:04:15 +0100

    Add routing by gameId to checkers game

--- checkers-game/src/App.tsx
+++ checkers-game/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import GameBoard from "@/components/GameBoard/index.tsx";
 import SecurityContextProvider from "@/context/SecurityContextProvider.tsx";
 import {RouteGuard} from "@/context/RouteGuard.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
@@ -9,9 +10,19 @@ function App() {
     return (
 
         <SecurityContextProvider>
-            <RouteGuard>
-                <GameBoard/>
-            </RouteGuard>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/:game_uuid"
+                        element={
+                            <RouteGuard>
+                                <GameBoard/>
+                            </RouteGuard>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+
         </SecurityContextProvider>
     )
 }
