8e8418fc57b93fd47b9431dfd5b2545cb9c09849
anna.deineko <anna.deineko@student.kdg.be>
2024-12-16 13:00:39 +0100

    upd: FC refactoring to function

--- bandit-games/src/components/Media/index.tsx
+++ bandit-games/src/components/Media/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-
-const Media: React.FC<> = () => {
+function Media() {
     return (
         <div className="flex items-center justify-center bg-purple-950 rounded-md h-72">
             <p className="text-gray-300">Game Media Placeholder</p>
