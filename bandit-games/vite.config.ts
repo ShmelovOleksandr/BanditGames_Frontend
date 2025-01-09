e84e4f5fab43cc3094fa04f0e6d5022ed856c6a2
Mykhailo Kruts <mykhailo.kruts@student.kdg.be>
2025-01-09 22:04:50 +0100

    Update config

--- bandit-games/vite.config.ts
+++ bandit-games/vite.config.ts
@@ -6,6 +6,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Ensure the build output is in build
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
